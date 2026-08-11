db838ee2d08f4195259aeae295ff7b4a95376b28
cultzg <cultz.pc@gmail.com>
2026-08-11 11:31:24 +0900

    jquery.tpl.js 수정

--- js/jquery.tpl.js
+++ js/jquery.tpl.js
@@ -1,20 +1,23 @@
 (function($){
-
-    $.fn.tpl = function(rows=[], tag='<ul>') {
-        let $el = $(this);
-        let tmpl = $el.html();
-        if(tag==='<table>')     {
-            tmpl = $().tpl.table(rows, tmpl);
-        } else if(tag==='<ul>') {
-            tmpl = $().tpl.rows(rows, tmpl||'<li>{_val}</li>');
+    $.fn.tpl = function(rows=[], tag='ul') {
+        var html = '';
+        const tmpl = $(this).html();
+        const slot = $(this).prop('slot')||this;
+        console.log(tmpl);
+        
+        if(tag==='table')     {
+            let th = '';//'<tr>'+$().tpl.row(rows[0], tmpl||'<th>{_key}</th>');
+            let tr = $().tpl.rows(rows, tmpl||'<td>{_val}</td>','<tr>');
+            html = $('<table>').append(th).append(tr);
         } else {
-            tmpl = $().tpl.rows(rows, tmpl||'<div>');
-        }                   
-        $el.html($(tag).append(tmpl));
+            let child = $().tpl.rows(rows, tmpl||'<li>{_val}</li>');
+            html = $(`<${tag}>`).append(child);
+        }                  
+        $(slot).html(html);
+        return this;
     }
-
-    $.fn.tpl.rows = function(rows, html){ //2차원 객체 배열처리
-        return rows.map(row => $().tpl.row(row, html)).join('');
+    $.fn.tpl.rows = function(rows, html, pre=''){ //2차원 객체 배열처리
+        return rows.map(row => pre+$().tpl.row(row, html)).join('');
     }
     $.fn.tpl.row = function(row, html){ //{$key}=>val로 변경 1차원 객체처리
         return $().tpl.replace(row, html);
@@ -23,10 +26,5 @@
         if( str.includes("{_key}") ) return Object.keys(row).map(key => str.replace(`{_key}`, key)).join('');
         if( str.includes("{_val}") ) return Object.keys(row).map(key => str.replace(`{_val}`, row[key])).join('');
         return Object.keys(row).reduce((acc, cur) => acc.replaceAll(`{${cur}}`, row[cur]), str);
-    }
-    $.fn.tpl.table = function(rows, str='<td>{_val}</td>') {
-        const th = '<tr>'+$().tpl.replace(rows[0], '<th>{_key}</th>');
-        const tr = rows.map(row => '<tr>'+$().tpl.replace(row, str)).join('');
-        return th + tr; 
     }
 }(jQuery));
