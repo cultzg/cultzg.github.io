d98c9183b3003be0c18297ae543a83feaddf00cc
cultzg <cultz.pc@gmail.com>
2026-09-18 16:24:38 +0900

    .

--- js/common.js
+++ js/common.js
@@ -9,11 +9,11 @@ const meta = `
     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
     <meta http-equiv="Cache-Control" content="no-cache,no-store,must-revalidate" />
     <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=yes">`;
-const cstyle = ['index.css'];
-const script = ['jquery.min.js'];
-
 const head = document.getElementsByTagName('head')[0];
 head.prepend(document.createRange().createContextualFragment(meta));
+
+const cstyle = ['index.css'];
+const script = ['jquery.min.js'];
 script.forEach(js=>{
     const script = document.createElement('script');
     script.type='text/javascript'
