13cad9c2822515299b3e01f835f04c0fc18ced9f
cultzg <cultz.pc@gmail.com>
2026-09-18 16:19:45 +0900

    .

--- js/common.js
+++ js/common.js
@@ -10,7 +10,7 @@
         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
         <meta http-equiv="Cache-Control" content="no-cache,no-store,must-revalidate" />
         <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=yes">`;
-    const cstyle = [];
+    const cstyle = ['index.css'];
     const script = ['jquery.min.js'];
 
     const head = document.getElementsByTagName('head')[0];
